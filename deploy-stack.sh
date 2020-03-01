35c9ad7876bb7e620397d8e905f3972bedd19076
Luke <lukemcdonnl@gmail.com>
2020-03-01 11:33:04 +0930

    Initial Commit

---
+++ deploy-stack.sh
@@ -0,0 +1,3 @@
+docker stack deploy --prune -c deploy/docker-compose.yml server
+#docker container prune -f;
+#docker image prune -f --filter "until=24h"
---
+++ install-docker.sh
@@ -0,0 +1,12 @@
+# including docker sudo
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install curl gnupg-agent apt-transport-https software-properties-common ca-certificates -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get autoremove -y
+sudo apt-get clean -y
+sudo docker swarm init
+./deploy-stack.sh
