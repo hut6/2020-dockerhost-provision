84e27416b13ef00350c1b586734b5a37534e6f58
Luke <lukemcdonnl@gmail.com>
2020-03-01 11:40:15 +0930

    fix path

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -1,3 +1,3 @@
-docker stack deploy --prune -c deploy/docker-compose.yml server
+docker stack deploy --prune -c docker-compose.yml server
 #docker container prune -f;
 #docker image prune -f --filter "until=24h"
