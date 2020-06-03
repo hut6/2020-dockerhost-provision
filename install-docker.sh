5c76967503ad65582aeeea8c1fc340081ec5eba8
Luke <lukemcdonnl@gmail.com>
2020-06-03 10:39:46 +0930

    Add promtail config

--- install-docker.sh
+++ install-docker.sh
@@ -23,3 +23,26 @@ cat << EOF > /etc/docker/daemon.json
   }
 }
 EOF
+
+cat << EOF > promtail/docker-config.yaml
+{
+server:
+  http_listen_port: 9080
+  grpc_listen_port: 0
+
+positions:
+  filename: /tmp/positions.yaml
+
+clients:
+  - url: $LOKI_URL
+
+scrape_configs:
+  - job_name: system
+    static_configs:
+      - targets:
+          - localhost
+        labels:
+          job: varlogs
+          __path__: /var/log/*log
+}
+EOF
