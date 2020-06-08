65ce5b3848d22404e00173f26406a392863404eb
Luke <lukemcdonnl@gmail.com>
2020-06-08 11:30:46 +0930

    More tweaks

--- setup-env.sh
+++ setup-env.sh
@@ -16,6 +16,24 @@ EOF
 rm -rf /etc/docker/promtail
 cp -r promtail /etc/docker/promtail
 chmod 655 /etc/docker/promtail/entry.sh
+mkdir /etc/docker/prometheus
+
+cat << EOF > /etc/docker/prometheus/prometheus.yml
+global:
+    scrape_interval:     5s
+    evaluation_interval: 20s
+
+remote_write:
+  - url: "${CORTEX_URL}"
+
+scrape_configs:
+  - job_name: 'Host Metrics'
+    static_configs:
+      - targets: ['node_exporter:9100']
+        labels:
+          host: ${HOST}
+
+EOF
 
 echo "${LOKI_URL}" > /etc/docker/promtail/LOKI_URL
 echo "${HOST}" > /etc/docker/promtail/HOST
