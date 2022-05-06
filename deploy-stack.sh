7f0cdd3aa0b1f380c53b2b01595c9f65881be19f
Luke McDonnell <lukemcdonnl@gmail.com>
2022-05-06 13:12:14 +0930

    isolate node services

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -2,5 +2,5 @@
 
 export $(cat .env | xargs)
 
-docker network create --scope=local --driver=bridge node_bridge | true
+#docker network create --scope=local --driver=bridge node_bridge | true
 docker stack deploy --prune -c docker-compose.yml server-"${HOST//./-}"
